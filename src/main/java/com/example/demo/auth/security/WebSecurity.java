052e4dca2fd45424be6cd4c8b28bcf000ea0f864
Cemal Kılıç <cemalkilic96@gmail.com>
2018-10-30 00:04:40 +0300

    Permit the home page "/" to be viewed.

--- src/main/java/com/example/demo/auth/security/WebSecurity.java
+++ src/main/java/com/example/demo/auth/security/WebSecurity.java
@@ -29,6 +29,7 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.cors().and().csrf().disable().authorizeRequests()
                 .antMatchers(HttpMethod.POST, SIGN_UP_URL).permitAll()
+                .antMatchers("/").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .addFilter(new JWTAuthenticationFilter(authenticationManager()))
