5eada1e149d9f891b93c706de404505483bd0917
Cemal Kılıç <cemalkilic96@gmail.com>
2018-11-01 23:54:15 +0300

    Modularized database connection code.

--- app.py
+++ app.py
@@ -1,36 +1,49 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, g
 import sqlite3 as sql
 
-from json import dumps
 from flask_classful import FlaskView, route
 
 app = Flask(__name__)
 
+DATABASE = 'bil495-abc.db'
+
+
+def get_db():
+    db = getattr(g, '_database', None)
+    if db is None:
+        db = g._database = sql.connect(DATABASE)
+    return db
+
+
+@app.teardown_appcontext
+def close_connection(exception):
+    db = getattr(g, '_database', None)
+    if db is not None:
+        db.close()
+
+
+def query_db(query, args=(), one=False):
+    cur = get_db().execute(query, args)
+    rv = cur.fetchall()
+    cur.close()
+    return (rv[0] if rv else None) if one else rv
+
 
 class InternalView(FlaskView):
-    def __init__(self):
-        self.con = sql.connect("bil495-abc.db", check_same_thread=False)
-        self.con.row_factory = sql.Row
-        self.cur = self.con.cursor()
-
     @app.route('/add')
     def add(self):
-        self.cur.execute(
-            'INSERT INTO PHOTO (photo_path, upload_date, uploaded_by)  VALUES("afafddasf", "2018-03-30", 1)')
-        rows = self.cur.fetchall();
+        rows = query_db('INSERT INTO PHOTO (photo_path, upload_date, uploaded_by)  VALUES("afafddasf", "2018-03-30", 1)')
+
         return app.response_class(str(rows), status=200)
 
     @app.route('/list')
     def list(self):
-        self.cur.execute("select * from photo")
-
-        rows = self.cur.fetchall();
-        for row in rows:
-            print(row)
+        rows = query_db('select * from photo')
+        print(rows)
 
         return app.response_class(str(rows), status=200)
 
 
 InternalView.register(app)
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=False)
+    app.run(debug=False)
