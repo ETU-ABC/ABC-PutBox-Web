d3205e6d214426fe2047dea1e0cd35a9a86d7da2
bahadircolak44 <bahdrcolak@gmail.com>
2018-11-24 15:07:39 +0100

    Define Like model #12

--- putbox/photos/models.py
+++ putbox/photos/models.py
@@ -8,12 +8,21 @@ class Photo(db.Model):
     upload_date = db.Column(db.DateTime, default=db.func.current_timestamp())
     uploaded_by = db.Column(db.Integer, db.ForeignKey('users.user_id'))
     tags = db.relationship("Tag")
+    liked_by = db.relationship("Like")
     album_id = db.Column(db.Integer, db.ForeignKey('album.album_id'))
-
     def __init__(self, photo_path, uploaded_by, album_id):
         self.photo_path = photo_path
         self.uploaded_by = uploaded_by
         self.album_id = album_id
+
+class Like(db.Model):
+    like_is = db.Column(db.Integer, primary_key=True)
+    photo_id = db.Column(db.Integer, db.ForeignKey('photo.photo_id'))
+    liked_by = db.Column(db.String, db.ForeignKey('user.username'))
+
+    def __init__(self, photo_id, liked_by):
+        self.photo_id = photo_id
+        self.liked_by = liked_by
 
 
 class Tag(db.Model):
